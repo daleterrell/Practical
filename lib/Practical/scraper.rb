302237190cd511bdc3a28288ec5ed3bf9dd0da80
Dale Terrell <tragicid@gmail.com>
2019-08-27 13:07:15 +0000

    stupid scraping

--- lib/Practical/scraper.rb
+++ lib/Practical/scraper.rb
@@ -1,9 +1,30 @@
 require 'nokogiri'
 require 'open-uri'
+site = "https://www.imdb.com/list/ls074044963/"
 
-html = open("https://www.imdb.com/list/ls074044963/")
+page = Nokogiri::HTML(open(site))
 
-Nokogiri::HTML(html)
+movies = page.css("div.lister-list .lister-item") 
 
-doc = Nokogiri::HTML(html)
-doc.css("<h3 class="lister-item-header").text.strip
+puts movies.count
+
+# things.each do |thing|
+#   title = thing.css("div.title").text.strip
+#   puts title
+# end
+
+ #enter a selector that you know you want to use in your scrape - i.e. div.main-content  or h1.title 
+
+#if the result is an empty array [], the content you want is probably being loaded with javascript
+
+
+#.text works on an array
+#['href'] does NOT work on an array 
+
+
+#if it's a really long result and you can't tell if the content you want is in there, try chaining .text to the end.
+  #for example: 
+    #page.css("div.main-content").text  OR  #page.css("div.main-content")[0].text
+
+
+ "Finished running code"
