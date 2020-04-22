6dc02c42d375e2061db0bc6a547d088f1ac106b1
Dale Terrell <tragicid@gmail.com>
2020-04-22 14:37:43 -0500

    Update cli.rb

--- lib/Practical/cli.rb
+++ lib/Practical/cli.rb
@@ -2,43 +2,77 @@ class Practical::CLI
 require 'nokogiri'
 require 'open-uri'
 
-  
-  def call 
-    Practical::Scraper.get_movies
- #   binding.pry
-    list_movies
+  def start
+    greeting
+    scrape_movies
     menu
-    goodbye
   end
   
-  def movies
-    @movies = Practical::Movies 
+#  def call 
+#    Practical::Scraper.get_movies
+#    binding.pry
+#    list_movies
+#    menu
+#    goodbye
+#  end
+  
+  def greeting
+    puts "Welcome to the world of practical effects in horror movies"
   end
   
-  def goodbye
-    puts "Enjoy.  We don't need CGI for this!!"
-  end
-  
-  def list_movies
-    puts "#{movies}"
-    @movies = Practical::Movies
-#    @movies.each.with_index(1) do {movies i}
-#    puts "#{i}, #{movies}"
+  def scrape_movies
+    Practical::Scraper.scrape_movies
   end
   
   def menu
-     input = nil 
-     while input != "exit"
-       puts "What number movie would you like more information about?"
-       input = gets.strip.downcase
-       if input.to_i > 0
-         puts @movies[input.to_i-1]
-       elsif input == "list"
-         list_movies
-     
-     puts "Would you like to see information on another movie? Enter Y or N"
-    end  
+    puts "If you want to list movies alphabetically, type 'abc'"
+    puts "If you want to list movies by rating, type 'rating'"
+    puts "If you want to exit, type 'exit'"
+
+    input = gets.strip.downcase
+
+    case input
+    when 'abc'
+      sort_movies_abc
+      list_movies
+      get_movie_method_in_loop_format
+    when 'rating'
+      sort_movies_rating
+      list_movies
+      get_movie_method_in_loop_format
+    when 'exit'
+    else
+      puts "Sorry I didn't understand that!"
+      menu
+    end
   end
+  
+  def sort_movies_abc
+    @sorted_movies = MovieReview::Movie.all.sort_by{|movie| movie.title}
+  end
+
+  def sort_movies_rating
+    @sorted_movies = MovieReview::Movie.all.reverse
+  end
+
+  def goodbye
+    puts "Enjoy.  We don't need CGI for this!!"
+  end
+ 
+  def list_movies
+    puts "Here are the top 100 movies:"
+    @sorted_movies.each.with_index(1) do |movie,index|
+      puts "#{index}. #{movie.title}"    # if index <= 100
+    end
+  end
+  
+  def list_movies
+    puts "Here are the top 100 movies:"
+    @sorted_movies.each.with_index(1) do |movie,index|
+      puts "#{index}. #{movie.title}"    # if index <= 100
+    end
+  end
+  
 end
 end
 
