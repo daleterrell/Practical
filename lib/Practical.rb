966cde5846437529f6a40835feba03df0f901ed7
Dale Terrell <tragicid@gmail.com>
2020-04-22 14:46:44 -0500

    Update Practical.rb

--- lib/Practical.rb
+++ lib/Practical.rb
@@ -1,10 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
 
-require_relative './Practical/version'
-require_relative './Practical/movie'
-require_relative './Practical/cli'
-require_relative './Practical/scraper'
+require_relative './Practical/version.rb'
+require_relative './Practical/movie.rb'
+require_relative './Practical/cli.rb'
+require_relative './Practical/scraper.rb'
+
+require_relative "movie_review/review.rb"
+
+
 
 
 
